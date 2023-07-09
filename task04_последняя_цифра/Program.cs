876768cec499adad713dc9405ecee7f0f82d98fc
A1ex82 <navitski82@gmail.com>
2023-07-09 13:16:28 +0300

    add tasks

---
+++ task04_последняя_цифра/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе
+ показывает последнюю цифру этого числа.
+ */
+
+ Console.WriteLine("Введите трехзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int integer = 0;
+if (number < 100 || number > 999) Console.WriteLine ("Трехзначное!!!");
+else {
+integer = number % 10;
+Console.WriteLine(integer);
+}
