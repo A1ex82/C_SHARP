43692d8568cced7f7df4212374561e79e508030a
A1ex82 <navitski82@gmail.com>
2023-07-09 16:50:53 +0300

    add tasks

---
+++ task_08_чётные_числа/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.
+*/
+
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Четные числа от 1 до N:");
+
+int count = 2;
+while (count <= N)
+{
+Console.Write(count + " ");
+count += 2;
+}
+
