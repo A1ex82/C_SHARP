5bff0b837fc6337cd7520fdf95f3221b5add2b17
A1ex82 <navitski82@gmail.com>
2023-07-12 20:03:02 +0300

    add tasks

---
+++ task_18/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+
+Console.WriteLine("Enter number of quarter ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void GetQuarter (int quarter)
+{
+if (quarter == 1)
+{
+Console.WriteLine("X and Y > 0");
+}
+else if (quarter == 2)
+{
+Console.WriteLine("X < 0 and Y > 0");
+}
+else if (quarter == 3)
+{
+Console.WriteLine("X < 0 and Y < 0");
+}
+else if (quarter == 4)
+{
+Console.WriteLine("X > 0 and Y < 0");
+}
+}
+
+string QuadrantRange(int quadrant);
+{
+if (quadrant == 1) return "x > 0; y > 0";
+else if (quadrant == 2) return "x < 0; y > 0";
+else if (quadrant == 3) return "x < 0; y < 0";
+else if (quadrant == 4) return "x > 0; y < 0";
+else return "invalid quadrant";
+}
+Console.Write("Enter a quadrant number: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(QuadrantRange(quarter));
---
+++ task_19/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+*/
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0 && number < 100000)
+{
+
+  int originalNumber = number;
+  int reversedNumber = 0;
+
+  while (number > 0)
+  {
+    int remainder = number % 10;
+    reversedNumber = reversedNumber * 10 + remainder;
+    number /= 10;
+  }
+
+  if (reversedNumber == originalNumber)
+  {
+    Console.WriteLine("Данное число является палиндромом.");
+  }
+  else
+  {
+    Console.WriteLine("Данное число не является палиндромом.");
+  }
+}
+else
+{
+  Console.WriteLine("Введите пятизначное число: ");
+}
