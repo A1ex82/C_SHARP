a5909b08b3b12ae998d1602cf9b8436983ea7d14
A1ex82 <navitski82@gmail.com>
2023-07-12 23:53:32 +0300

    add tasks

---
+++ task_28/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+int Base(int from, int to)
+{
+int product = 1;
+for (int i = from; i <= to; i++)
+{
+product *= i;
+}
+return product;
+}
+
+Console.WriteLine("Введите ваше число:");
+int value = Convert.ToInt32(Console.ReadLine());
+int result= Base(1,value);
+Console.WriteLine($"Ответ: {result}");
---
+++ task_29_hw/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+
+void PrintArray(int[] array)
+{
+  Console.Write("[");
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write(array[i]);
+    if (i < array.Length - 1)
+    {
+      Console.Write(", ");
+    }
+  }
+  Console.Write("]");
+}
+
+Console.WriteLine("Введите 8 целых чисел:");
+
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+PrintArray(array);
