955df9738f12fbceaf5ab6080aa2d7d3a557a030
A1ex82 <navitski82@gmail.com>
2023-07-16 12:34:27 +0300

    add tasks

---
+++ task_41/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+
+Console.Write("Введите количество чисел M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int countPositive = 0;
+
+for (int i = 0; i < M; i++)
+{
+  Console.Write($"Введите число {i + 1}: ");
+  int number = Convert.ToInt32(Console.ReadLine());
+
+  if (number > 0)
+    countPositive++;
+}
+
+Console.WriteLine($"Количество чисел больше 0: {countPositive}");
