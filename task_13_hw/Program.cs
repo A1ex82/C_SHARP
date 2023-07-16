5ffce9c1b6c9dd3dae82199ca3a2d26beb667c8d
A1ex82 <navitski82@gmail.com>
2023-07-16 19:28:30 +0300

    add tasks

---
+++ task_13_hw/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int thirdDigit = GetThirdDigit(number);
+if (thirdDigit != -1)
+{
+  Console.WriteLine($"Третья цифра числа: {thirdDigit}");
+}
+else
+{
+  Console.WriteLine("Третьей цифры нет.");
+}
+int GetThirdDigit(int number)
+{
+  if (number >= 100 && number <= 999)
+  {
+    return (number / 100) % 10;
+  }
+  return -1;
+}
---
+++ task_15_hw/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введите цифру дня недели (1 - понедельник, 2 - вторник, ..., 7 - воскресенье): ");
+int dayOfWeek = int.Parse(Console.ReadLine());
+
+bool isWeekend = IsWeekend(dayOfWeek);
+if (isWeekend)
+{
+  Console.WriteLine("Этот день является выходным.");
+}
+else
+{
+  Console.WriteLine("Этот день не является выходным.");
+}
+bool IsWeekend(int dayOfWeek)
+{
+  return dayOfWeek == 6 || dayOfWeek == 7;
+}
