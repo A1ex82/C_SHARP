fe35d6eab127263201d6c12bd04459d2be735fa7
A1ex82 <navitski82@gmail.com>
2023-07-23 22:22:19 +0300

    add task

---
+++ task_63_hw/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+void recCount(int count, int n)
+{
+  if (count <= n)
+  {
+    Console.WriteLine(count);
+    recCount(count + 1, n);
+  }
+}
+
+recCount(1, 5);
